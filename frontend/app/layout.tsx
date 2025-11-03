fc35d838a869219ecb0523b4c2747b89d1d54773
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2025-11-03 14:10:07 +0200

    implement the components into the UI

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/ui/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        style={{ background: "fafafa" }}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Sidebar />
         {children}
       </body>
     </html>
