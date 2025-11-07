665dccffbb7dd03c3010dc5be2b67847214687d8
Marius Achim <mariusachim2004@gmail.com>
2025-11-07 12:40:02 +0200

    Rearanged '__init__' file.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,11 +7,11 @@ def create_app():
   app = Flask(__name__)
   CORS(app)
 
+  # Allow all headers & methods for CORS
+  CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
   # Register routes
   app.register_blueprint(car_routes, url_prefix='/api/v1/cars')
   app.register_blueprint(city_routes, url_prefix='/api/v1/cities')
-
-  # Allow all headers & methods for CORS
-  CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
   return app
