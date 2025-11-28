5f0782bc9695a3f8130e411e0caabb3e96c22c54
AlexUrluescu <urluescualex2003@gmail.com>
2025-11-28 20:12:43 +0200

    create new page Admin into another route

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from app.routes.v1.car_routes import car_routes
 from app.routes.v1.render_home import render_home_page
+from app.routes.v1.render_admin import render_admin_page
 from app.routes.v1.city_routes import city_routes
 from app.routes.v1.population_routes import population_routes
 from app.routes.v1.pollution_routes import pollution_routes
@@ -22,6 +23,7 @@ def create_app():
   app.register_blueprint(parking_spots_routes, url_prefix='/api/v1/parking_spots')
   app.register_blueprint(traffic_routes_home, url_prefix="/api/v1/traffic-chat-bot-home")
   app.register_blueprint(render_home_page, url_prefix="/")
+  app.register_blueprint(render_admin_page, url_prefix="/admin")
 
   return app
 
